e3e2a039cc94af11bfd4fa02c2e2716edfa072a0
Khoa <khoa.vt@aris-vn.com>
2015-08-25 19:13:10 +0700

    ! create project

---
+++ Sloom/Sloom/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sloom
+//
+//  Created by khoa.vt on 8/25/15.
+//  Copyright (c) 2015 khoa.vt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sloom/Sloom/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sloom
+//
+//  Created by khoa.vt on 8/25/15.
+//  Copyright (c) 2015 khoa.vt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
